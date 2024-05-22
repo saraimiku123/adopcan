05a0fa44d9bab76c086fac440bed5958b4b3e639
saraimiku123 <hatsunekazamy@gmail.com>
2024-05-22 01:59:48 -0600

    login y creacion de cuentas validadas 21-05-2024

--- app/src/main/java/com/example/adopcan/Login.java
+++ app/src/main/java/com/example/adopcan/Login.java
@@ -5,16 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText  campo_correo,campo_contrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final Button iniciar_sesiones = findViewById(R.id.iniciar_sesion);
+        campo_correo = findViewById(R.id.correo);
+        campo_contrasenia= findViewById(R.id.password);
         final TextView registrarse = findViewById(R.id.registrarse);
 
+        iniciar_sesiones.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if (validar()) {
+                    Toast.makeText(Login.this, "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Login.this, Prueba.class));
+                    finish();
+                }
+
+            }
+        });
+       // etiqueta de link personas que no tienen cuenta
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,4 +43,35 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
+
+
+public boolean validar() {
+    boolean retorno = true;
+    String entrada_correo = campo_correo.getText().toString();
+    String entrada_contrasenia = campo_contrasenia.getText().toString();
+
+    String regexCorreo = "^[a-zA-Z0-9._%+-]+@(gmail\\.com|hotmail\\.com|@outllok\\.com)$";
+    String regexContrasenia = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]{8,}$";
+
+
+
+    if (entrada_correo.isEmpty()) {
+        campo_correo.setError("El correo no puede quedar vacío");
+        retorno = false;
+    } else if (!entrada_correo.matches(regexCorreo)) {
+        campo_correo.setError("El correo debe contener letras, números y caracteres especiales comunes, y terminar en '@.com'");
+        retorno = false;
+    }
+    if (entrada_contrasenia.isEmpty()) {
+        campo_contrasenia.setError("La contraseña no puede quedar vacía");
+        retorno = false;
+    } else if (!entrada_contrasenia.matches(regexContrasenia)) {
+        campo_contrasenia.setError("La contraseña debe tener al menos 8 caracteres, incluyendo letras mayúsculas, minúsculas, números y caracteres especiales");
+        retorno = false;
+
+    }
+
+    return retorno;
+}
 }
--- app/src/main/java/com/example/adopcan/RegistroUsuarios.java
+++ app/src/main/java/com/example/adopcan/RegistroUsuarios.java
@@ -6,21 +6,79 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistroUsuarios extends AppCompatActivity {
+
+    EditText campo_nombre,campo_telefono, campo_correo,campo_contrasenia;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuarios);
         final Button crearuser = findViewById(R.id.crearuser);
+        campo_nombre = findViewById(R.id.namecrear);
+        campo_telefono= findViewById(R.id.telefono);
+        campo_correo = findViewById(R.id.correo);
+        campo_contrasenia= findViewById(R.id.contraseniatext);
+
+
+
+
+
 
         crearuser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RegistroUsuarios.this, Prueba.class));
-                finish();
+                if (validar()) {
+                    Toast.makeText(RegistroUsuarios.this, "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegistroUsuarios.this, Prueba.class));
+                    finish();
+                }
             }
         });
+    }
+
+    public boolean validar() {
+        boolean retorno = true;
+        String entrada_nombre = campo_nombre.getText().toString();
+        String entrada_telefono = campo_telefono.getText().toString();
+        String entrada_correo = campo_correo.getText().toString();
+        String entrada_contrasenia = campo_contrasenia.getText().toString();
+
+        String regexCorreo = "^[a-zA-Z0-9._%+-]+@(gmail\\.com|hotmail\\.com|@outllok\\.com)$";
+        String regexTelefono = "^\\+\\d{1,3}\\d{10}$";
+        String regexContrasenia = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]{8,}$";
+
+        if (entrada_nombre.isEmpty()) {
+            campo_nombre.setError("El nombre no puede quedar vacío");
+            retorno = false;
+        }
+        if (entrada_telefono.isEmpty()) {
+            campo_telefono.setError("El teléfono no puede quedar vacío");
+            retorno = false;
+        } else if (!entrada_telefono.matches(regexTelefono)) {
+            campo_telefono.setError("El teléfono debe incluir lada (1-3 dígitos) seguida de un número (10 dígitos)\"");
+            
+        }
+        if (entrada_correo.isEmpty()) {
+            campo_correo.setError("El correo no puede quedar vacío");
+            retorno = false;
+        } else if (!entrada_correo.matches(regexCorreo)) {
+            campo_correo.setError("El correo debe contener letras, números y caracteres especiales comunes, y terminar en '@.com'");
+            retorno = false;
+        }
+        if (entrada_contrasenia.isEmpty()) {
+            campo_contrasenia.setError("La contraseña no puede quedar vacía");
+            retorno = false;
+        } else if (!entrada_contrasenia.matches(regexContrasenia)) {
+            campo_contrasenia.setError("La contraseña debe tener al menos 8 caracteres, incluyendo letras mayúsculas, minúsculas, números y caracteres especiales");
+            retorno = false;
+
+        }
+
+        return retorno;
     }
 }
