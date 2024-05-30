7521f6ccf410ee46224fe27e53fc36d13e750066
saraimiku123 <hatsunekazamy@gmail.com>
2024-05-30 10:25:43 -0600

    "inicio y registro validados en base de datos"

--- app/src/main/java/com/example/adopcanapp/LoginActivity.java
+++ app/src/main/java/com/example/adopcanapp/LoginActivity.java
@@ -11,9 +11,21 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
 
-    EditText campo_correo,campo_contrasenia;
+    EditText campo_correo, campo_contrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +34,24 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-
         final Button iniciar_sesiones = findViewById(R.id.iniciar_sesion);
         campo_correo = findViewById(R.id.correo);
-        campo_contrasenia= findViewById(R.id.password);
+        campo_contrasenia = findViewById(R.id.password);
         final TextView registrarse = findViewById(R.id.registrarse);
 
 
-        iniciar_sesiones.setOnClickListener(new View.OnClickListener(){
+        iniciar_sesiones.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                if (validar()) {
-                    Toast.makeText(LoginActivity.this, "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(LoginActivity.this, MenuActivity.class));
-                    finish();
-                }
+//                if (validar()) {
+//                    Toast.makeText(LoginActivity.this, "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+//                    finish();
+//                }
+                validarUsuario("https://adopcan.000webhostapp.com/adopcion_login.php");
+
+                validar();
 
 
             }
@@ -46,12 +60,11 @@ public class LoginActivity extends AppCompatActivity {
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            startActivity(new Intent(LoginActivity.this,RegistroUsuariosActivity.class));
+                startActivity(new Intent(LoginActivity.this, RegistroUsuariosActivity.class));
 
             }
         });
     }
-
 
 
     public boolean validar() {
@@ -61,7 +74,6 @@ public class LoginActivity extends AppCompatActivity {
 
         String regexCorreo = "^[a-zA-Z0-9._%+-]+@(gmail\\.com|hotmail\\.com|@outllok\\.com)$";
         String regexContrasenia = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]{8,}$";
-
 
 
         if (entrada_correo.isEmpty()) {
@@ -83,5 +95,33 @@ public class LoginActivity extends AppCompatActivity {
         return retorno;
     }
 
-}
+    private void validarUsuario(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Verifica si la respuesta contiene "exito" para determinar que el inicio de sesión fue exitoso
+                if (!response.isEmpty()) {
+                    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Correo o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("correo", campo_correo.getText().toString());
+                parametros.put("contrasenia", campo_contrasenia.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
 
+}
--- app/src/main/java/com/example/adopcanapp/RegistroUsuariosActivity.java
+++ app/src/main/java/com/example/adopcanapp/RegistroUsuariosActivity.java
@@ -5,14 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -20,41 +15,38 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import java.util.HashMap;
 import java.util.Map;
+import com.android.volley.Response.ErrorListener;
+
 
 public class RegistroUsuariosActivity extends AppCompatActivity {
 
     EditText campo_nombre, campo_telefono, campo_correo, campo_contrasenia;
-    String tipo_usuario;
-
+//    Switch acepta_terminos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resgistro_usuarios);
+
         final Button crearuser = findViewById(R.id.crearuser);
         campo_nombre = findViewById(R.id.namecrear);
         campo_telefono = findViewById(R.id.telefono);
         campo_correo = findViewById(R.id.correo);
         campo_contrasenia = findViewById(R.id.contraseniatext);
-
+//        acepta_terminos=findViewById(R.id.switch_terminos);
 
         crearuser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (validar()) {
-                    Toast.makeText(RegistroUsuariosActivity.this, "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(RegistroUsuariosActivity.this, MenuActivity.class));
-                    finish();
                     ejecutarServicio("https://adopcan.000webhostapp.com/crearusuario_app.php");
                 }
-
             }
         });
-    }
 
+    }
     public boolean validar() {
         boolean retorno = true;
         String entrada_nombre = campo_nombre.getText().toString();
@@ -62,7 +54,7 @@ public class RegistroUsuariosActivity extends AppCompatActivity {
         String entrada_correo = campo_correo.getText().toString();
         String entrada_contrasenia = campo_contrasenia.getText().toString();
 
-        String regexCorreo = "^[a-zA-Z0-9._%+-]+@(gmail\\.com|hotmail\\.com|@outllok\\.com)$";
+        String regexCorreo = "^[a-zA-Z0-9._%+-]+@(gmail\\.com|hotmail\\.com|outlook\\.com)$";
         String regexTelefono = "^\\+\\d{1,3}\\d{10}$";
         String regexContrasenia = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]{8,}$";
 
@@ -74,14 +66,14 @@ public class RegistroUsuariosActivity extends AppCompatActivity {
             campo_telefono.setError("El teléfono no puede quedar vacío");
             retorno = false;
         } else if (!entrada_telefono.matches(regexTelefono)) {
-            campo_telefono.setError("El teléfono debe incluir lada (1-3 dígitos) seguida de un número (10 dígitos)\"");
-
+            campo_telefono.setError("El teléfono debe incluir lada (1-3 dígitos) seguida de un número (10 dígitos)");
+            retorno = false;
         }
         if (entrada_correo.isEmpty()) {
             campo_correo.setError("El correo no puede quedar vacío");
             retorno = false;
         } else if (!entrada_correo.matches(regexCorreo)) {
-            campo_correo.setError("El correo debe contener letras, números y caracteres especiales comunes, y terminar en '@.com'");
+            campo_correo.setError("El correo debe contener letras, números y caracteres especiales comunes, y terminar en '@gmail.com', '@hotmail.com' o '@outlook.com'");
             retorno = false;
         }
         if (entrada_contrasenia.isEmpty()) {
@@ -90,39 +82,41 @@ public class RegistroUsuariosActivity extends AppCompatActivity {
         } else if (!entrada_contrasenia.matches(regexContrasenia)) {
             campo_contrasenia.setError("La contraseña debe tener al menos 8 caracteres, incluyendo letras mayúsculas, minúsculas, números y caracteres especiales");
             retorno = false;
-
         }
 
         return retorno;
     }
 
-    public void ejecutarServicio(String URL){
-        StringRequest stringRequest= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+    public void ejecutarServicio(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Toast.makeText(getApplicationContext(), "Operacion Exitosa",Toast.LENGTH_SHORT).show();
+                if (!response.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Operación Exitosa", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegistroUsuariosActivity.this, MenuActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(RegistroUsuariosActivity.this, "Los datos no son los correctos", Toast.LENGTH_SHORT).show();
+                }
             }
-        } ,new Response.ErrorListener(){
-
+        }, new ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), "Error en la petición: " + error.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        }){
-            protected Map<String,String> getParams()throws AuthFailureError{
-                Map<String,String>parametros= new HashMap<String, String>();
-                parametros.put("correo",campo_correo.getText().toString());
-                parametros.put("telefono",campo_telefono.getText().toString());
-                parametros.put("nombre_completo",campo_nombre.getText().toString());
-                parametros.put("contrasenia",campo_contrasenia.getText().toString());
-                parametros.put("tipo_usuario",tipo_usuario="Usuario");
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("correo", campo_correo.getText().toString());
+                parametros.put("telefono", campo_telefono.getText().toString());
+                parametros.put("nombre_completo", campo_nombre.getText().toString());
+                parametros.put("contrasenia", campo_contrasenia.getText().toString());
+                parametros.put("tipo_usuario", "Usuario");
 
                 return parametros;
             }
         };
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
-
     }
-
 }
